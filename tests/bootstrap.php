b02f8e624c62444dfb4d2f92f9c4a9368958c177
Vasek Purchart <me@vasekpurchart.cz>
2025-01-28 14:59:18 +0100

    remove converting to Enum in postLoad event

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,6 @@
 
 declare(strict_types = 1);
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
 error_reporting(E_ALL);
 
-$loader = require __DIR__ . '/../vendor/autoload.php';
-
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+require __DIR__ . '/../vendor/autoload.php';
